b5d0c7cfc06bc82afa18573f4ce8f49232e7d290
josephruiz18101 <joseph123920@gmail.com>
2024-11-14 12:43:42 -0600

    removed code

--- models/profiles.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const profileSchema = new mongoose.Schema({
-    gamerTag: { type: String, required: true },
-    platform: { type: String, required: true },
-    games: { type: [String], required: true },
-});
-
-module.exports = mongoose.model('Profile', profileSchema);
